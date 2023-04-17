607970a8fc6f4ebaf05f4d923bde727f03d092dc
Cristian Stanciu <crististanciu095@gmail.com>
2023-04-17 23:08:56 +0300

    Refactor code

--- JS/Lab3JS3v2/script.js
+++ JS/Lab3JS3v2/script.js
@@ -12,32 +12,28 @@ Nu se vor folosi biblioteci de functii, jQuery, pluginuri, etc.
 
 console.log("Welcome to script.js!");
 
-function startGame(n, numberOfSeconds) {
-    var timer;
+function startTimer() {
+    const startTime = new Date().getTime();
+    const timerLabel = document.getElementById("timer-label");
 
-    function startTimer() {
-        const startTime = new Date().getTime();
-        const timerLabel = document.getElementById("timer-label");
+    return setInterval(() => {
+        const currentTime = new Date().getTime();
+        const elapsedTime = Math.floor((currentTime - startTime) / 1000);
 
-        timer = setInterval(() => {
-            const currentTime = new Date().getTime();
-            const elapsedTime = Math.floor((currentTime - startTime) / 1000);
+        const hours = Math.floor(elapsedTime / 3600);
+        const minutes = Math.floor((elapsedTime % 3600) / 60);
+        const seconds = elapsedTime % 60;
 
-            const hours = Math.floor(elapsedTime / 3600);
-            const minutes = Math.floor((elapsedTime % 3600) / 60);
-            const seconds = elapsedTime % 60;
+        const hoursStr = (hours < 10) ? (`0${hours}`) : (hours);
+        const minutesStr = (minutes < 10) ? (`0${minutes}`) : (minutes);
+        const secondsStr = (seconds < 10) ? (`0${seconds}`) : (seconds);
 
-            const hoursStr = (hours < 10) ? ("0" + hours) : (hours);
-            const minutesStr = (minutes < 10) ? ("0" + minutes) : (minutes);
-            const secondsStr = (seconds < 10) ? ("0" + seconds) : (seconds);
+        timerLabel.innerHTML = `<em>Time</em>: <strong>${hoursStr}:${minutesStr}:${secondsStr}</strong>`;
+    }, 1000);
+}
 
-            timerLabel.innerHTML = `<em>Time</em>: <strong>${hoursStr}:${minutesStr}:${secondsStr}</strong>`;
-        }, 1000);
-    }
-
-    startTimer();
-
-    const imageData = [
+function getImages() {
+    return [
         { src: "./Images/image0.jpg", alt: "Image 0" },
         { src: "./Images/image1.jpg", alt: "Image 1" },
         { src: "./Images/image2.jpg", alt: "Image 2" },
@@ -58,41 +54,45 @@ function startGame(n, numberOfSeconds) {
         { src: "./Images/image17.jpg", alt: "Image 17" },
         { src: "./Images/image18.jpg", alt: "Image 18" }
     ];
+}
 
-    function getRandomNumber(n) {
-        return Math.floor(Math.random() * n);
-    }
+function getRandomNumber(n) {
+    return Math.floor(Math.random() * n);
+}
 
+function getNumbersArray(n) {
     const numbers = [];
-
     for(let i = 1; i <= n * n / 2; ++i) {
         numbers.push(i);
         numbers.push(i);
     }
+    return numbers;
+}
 
-    function deleteElement(arr, index) {
-        if(index !== -1) {
-            arr.splice(index, 1);
-        }
+function deleteElement(arr, index) {
+    if(index !== -1) {
+        arr.splice(index, 1);
     }
+}
 
+function getRandomNumbersArray(numbers) {
     const randomNumbersArray = [];
-
     while(numbers.length !== 0) {
         const randomIndex = getRandomNumber(numbers.length);
         const randomNumber = numbers[randomIndex];
         randomNumbersArray.push(randomNumber);
         deleteElement(numbers, randomIndex);
     }
+    return randomNumbersArray;
+}
 
+function computeGameTable(n, randomNumbersArray, matrix, visibleMatrix, imageData) {
     const table = document.getElementById("game-table");
-    var matrix = [];
-    var visibleMatrix = [];
-    var currentElemIndex = 0;
+    let currentElemIndex = 0;
 
     for(let r = 0; r < n; ++r) {
-        var row = [];
-        var visibleMatrixRow = [];
+        const row = [];
+        const visibleMatrixRow = [];
 
         const newRow = table.insertRow();
         for(let c = 0; c < n; ++c) {
@@ -111,31 +111,45 @@ function startGame(n, numberOfSeconds) {
         visibleMatrix.push(visibleMatrixRow);
     }
 
+    return table;
+}
+
+function hideCell(r, c, imageData) {
+    const cellID = `row${r}col${c}`;
+    const cell = document.getElementById(cellID);
+    cell.innerHTML = `<img src="${imageData[0].src}" alt="${imageData[0].alt}" width="50" height="50">`;
+}
+
+function updateNumberOfMoves(numberOfMoves) {
+    const numberOfMovesLabel = document.getElementById("moves-label");
+    numberOfMovesLabel.innerHTML = `<em>Score</em>: <strong>${++numberOfMoves}</strong>`;
+    return numberOfMoves;
+}
+
+function startGame(n, numberOfSeconds) {
+    const timer = startTimer();
+    const imageData = getImages();
+    const numbers = getNumbersArray(n);
+    const randomNumbersArray = getRandomNumbersArray(numbers);
+
+    const matrix = [];
+    const visibleMatrix = [];
+    const table = computeGameTable(n, randomNumbersArray, matrix, visibleMatrix, imageData);
+
     console.log(matrix);
 
-    function hideCell(r, c) {
-        const cellID = "row" + r + "col" + c;
-        const cell = document.getElementById(cellID);
-        cell.innerHTML = `<img src="${imageData[0].src}" alt="${imageData[0].alt}" width="50" height="50">`;
-    }
+    let firstCellClickedVal = false;
+    let firstCellClickedRow = -1;
+    let firstCellClickedCol = -1;
 
-    function updateNumberOfMoves() {
-        const numberOfMovesLabel = document.getElementById("moves-label");
-        numberOfMovesLabel.innerHTML = `<em>Score</em>: <strong>${++numberOfMoves}</strong>`;
-    }
+    let secondCellClickedVal = false;
+    let secondCellClickedRow = -1;
+    let secondCellClickedCol = -1;
 
-    var firstCellClickedVal = false;
-    var firstCellClickedRow = -1;
-    var firstCellClickedCol = -1;
-
-    var secondCellClickedVal = false;
-    var secondCellClickedRow = -1;
-    var secondCellClickedCol = -1;
-
-    var hiddenElements = n * n;
-    var win = false;
-    var blocked = false;
-    var numberOfMoves = 0;
+    let hiddenElements = n * n;
+    let win = false;
+    let blocked = false;
+    let numberOfMoves = 0;
 
     table.addEventListener("click", function(event) {
         if(blocked) {
@@ -144,11 +158,11 @@ function startGame(n, numberOfSeconds) {
 
         if(win === false) {
             const cell = event.target;
-            var rowIndex = undefined;
-            var colIndex = undefined;
+            let rowIndex = undefined;
+            let colIndex = undefined;
 
             if(cell.tagName === "IMG") {
-                const tableCell = cell.closest('td');
+                const tableCell = cell.closest("td");
 
                 const row = tableCell.parentNode;
                 rowIndex = row.rowIndex;
@@ -162,9 +176,9 @@ function startGame(n, numberOfSeconds) {
             }
 
             if(cell.tagName === "IMG" || cell.tagName === "TD") {
-                var validMove = false;
+                let validMove = false;
 
-                const clickedCellID = "row" + rowIndex + "col" + colIndex;
+                const clickedCellID = `row${rowIndex}col${colIndex}`;
                 const clickedCell = document.getElementById(clickedCellID);
                 const clickedImageIndex = matrix[rowIndex][colIndex];
                 clickedCell.innerHTML = `<img src="${imageData[clickedImageIndex].src}" alt="${imageData[clickedImageIndex].alt}" width="50" height="50">`;
@@ -188,8 +202,8 @@ function startGame(n, numberOfSeconds) {
                     secondCellClickedCol = colIndex;
 
                     console.clear();
-                    console.log("First cell clicked value: " + firstCellClickedVal);
-                    console.log("Second cell clicked value: " + secondCellClickedVal);
+                    console.log(`First cell clicked value: ${firstCellClickedVal}`);
+                    console.log(`Second cell clicked value: ${secondCellClickedVal}`);
 
                     if(firstCellClickedVal === secondCellClickedVal) {
                         if (firstCellClickedRow !== secondCellClickedRow || firstCellClickedCol !== secondCellClickedCol) {
@@ -205,7 +219,7 @@ function startGame(n, numberOfSeconds) {
 
                             blocked = true;
                             setTimeout(function() {
-                                hideCell(firstCellClickedRow, firstCellClickedCol);
+                                hideCell(firstCellClickedRow, firstCellClickedCol, imageData);
                                 blocked = false;
                             }, numberOfSeconds * 1000);
                         }
@@ -215,8 +229,8 @@ function startGame(n, numberOfSeconds) {
 
                         blocked = true;
                         setTimeout(function() {
-                            hideCell(firstCellClickedRow, firstCellClickedCol);
-                            hideCell(secondCellClickedRow, secondCellClickedCol);
+                            hideCell(firstCellClickedRow, firstCellClickedCol, imageData);
+                            hideCell(secondCellClickedRow, secondCellClickedCol, imageData);
                             blocked = false;
                         }, numberOfSeconds * 1000);
 
@@ -227,7 +241,7 @@ function startGame(n, numberOfSeconds) {
                 }
 
                 if(validMove) {
-                    updateNumberOfMoves();
+                    numberOfMoves = updateNumberOfMoves(numberOfMoves);
                 }
             }
         }
