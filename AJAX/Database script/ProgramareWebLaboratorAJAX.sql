b544bb77b121c3713afb3962f13af021424b3750
Cristian Stanciu <crististanciu095@gmail.com>
2023-05-18 16:10:16 +0300

    Updated MySQL script
    Now the script contains the table "clienti" used for the second problem.

--- AJAX/Database script/ProgramareWebLaboratorAJAX.sql
+++ AJAX/Database script/ProgramareWebLaboratorAJAX.sql
@@ -12,7 +12,7 @@ CREATE TABLE `trenuri` (
     CONSTRAINT CHECK (`plecare` <> `sosire`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `trenuri` (`plecare`, `sosire`) VALUES
+INSERT INTO `trenuri` VALUES
 ('Cluj-Napoca', 'Bucuresti'),
 ('Iasi', 'Suceava'),
 ('Timisoara', 'Cluj-Napoca'),
@@ -41,3 +41,28 @@ FROM `trenuri`
 UNION
 SELECT `sosire`
 FROM `trenuri`;
+
+CREATE TABLE `clienti` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+	`nume` NVARCHAR(40) NOT NULL,
+	`prenume` NVARCHAR(40) NOT NULL,
+    `telefon` VARCHAR(20) NOT NULL UNIQUE,
+    `email` VARCHAR(30) NOT NULL UNIQUE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `clienti` (`nume`, `prenume`, `email`, `telefon`) VALUES
+('Bast', 'Alcmene', 'balcmene13@gmail.com', '(206) 342-8631'),
+('Níam', 'Simon', 'nsimon25@yahoo.com', '(717) 550-1675'),
+('Metis', 'Aella', 'maella8@gmail.com', '(248) 762-0356'),
+('Alfr', 'Gerðr', 'alfr99@gmail.com', '(253) 644-2182'),
+('Praxis', 'Aeolus', 'prazis@yahoo.com', '(212) 658-3916'),
+('Parthalán', 'Anthea', 'panthea18@outlook.com', '(209) 300-2557'),
+('Arachne', 'Phrixos', 'aphrixos5@yahoo.com', '(262) 162-1585'),
+('Leda', 'Hoder', 'leda_hoder89@gmail.com', '(252) 258-3799'),
+('Clíodhna', 'Kore', 'ckore27@outlook.com', '(234) 109-6666'),
+('Rigantona', 'Erato', 'rigantona_erato@gmail.com', '(201) 874-8593'),
+('Yngvi', 'Diomedes', 'ydiomedes@gmail.com', '(815) 922-6178');
+
+SELECT * FROM `clienti`;
+
+SELECT COUNT(*) AS `numar clienti` FROM `clienti`;
