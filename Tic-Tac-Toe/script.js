d64f8181d2729cb0facca75c21c8f4001dfd81e1
Cristian Stanciu <crististanciu095@gmail.com>
2023-05-19 13:55:54 +0300

    Improved the difficulty of the game
    Now the moves of the computer will be random except the situation when the computer or the player can win the game (by performing a decisive move).

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -134,6 +134,157 @@ function gameIsOver() {
     return false;
 }
 
+function computeSmartMove(symbol) {
+    const cell11 = document.querySelector("#cell11");
+    const cell12 = document.querySelector("#cell12");
+    const cell13 = document.querySelector("#cell13");
+
+    const cell21 = document.querySelector("#cell21");
+    const cell22 = document.querySelector("#cell22");
+    const cell23 = document.querySelector("#cell23");
+
+    const cell31 = document.querySelector("#cell31");
+    const cell32 = document.querySelector("#cell32");
+    const cell33 = document.querySelector("#cell33");
+
+    // ROWS/LINES CHECK
+    // FIRST ROW/LINE
+    if (cell11.textContent === cell12.textContent && cell11.textContent === symbol && cell13.textContent === "-") {
+        cell13.textContent = computer;
+        return true;
+    }
+
+    if (cell11.textContent === cell13.textContent && cell11.textContent === symbol && cell12.textContent === "-") {
+        cell12.textContent = computer;
+        return true;
+    }
+
+    if (cell12.textContent === cell13.textContent && cell12.textContent === symbol && cell11.textContent === "-") {
+        cell11.textContent = computer;
+        return true;
+    }
+
+    // SECOND ROW/LINE
+    if (cell21.textContent === cell22.textContent && cell21.textContent === symbol && cell23.textContent === "-") {
+        cell23.textContent = computer;
+        return true;
+    }
+
+    if (cell21.textContent === cell23.textContent && cell21.textContent === symbol && cell22.textContent === "-") {
+        cell22.textContent = computer;
+        return true;
+    }
+
+    if (cell22.textContent === cell23.textContent && cell22.textContent === symbol && cell21.textContent === "-") {
+        cell21.textContent = computer;
+        return true;
+    }
+
+    // THIRD ROW/LINE
+    if (cell31.textContent === cell32.textContent && cell31.textContent === symbol && cell33.textContent === "-") {
+        cell33.textContent = computer;
+        return true;
+    }
+
+    if (cell31.textContent === cell33.textContent && cell31.textContent === symbol && cell32.textContent === "-") {
+        cell32.textContent = computer;
+        return true;
+    }
+
+    if (cell32.textContent === cell33.textContent && cell32.textContent === symbol && cell31.textContent === "-") {
+        cell31.textContent = computer;
+        return true;
+    }
+
+    // DIAGONALS CHECK
+    // FIRST DIAGONAL CHECK
+    if (cell11.textContent === cell22.textContent && cell11.textContent === symbol && cell33.textContent === "-") {
+        cell33.textContent = computer;
+        return true;
+    }
+
+    if (cell11.textContent === cell33.textContent && cell11.textContent === symbol && cell22.textContent === "-") {
+        cell22.textContent = computer;
+        return true;
+    }
+
+    if (cell22.textContent === cell33.textContent && cell22.textContent === symbol && cell11.textContent === "-") {
+        cell11.textContent = computer;
+        return true;
+    }
+
+    // SECOND DIAGONAL CHECK
+    if (cell13.textContent === cell22.textContent && cell13.textContent === symbol && cell31.textContent === "-") {
+        cell31.textContent = computer;
+        return true;
+    }
+
+    if (cell13.textContent === cell31.textContent && cell13.textContent === symbol && cell22.textContent === "-") {
+        cell22.textContent = computer;
+        return true;
+    }
+
+    if (cell22.textContent === cell31.textContent && cell22.textContent === symbol && cell13.textContent === "-") {
+        cell13.textContent = computer;
+        return true;
+    }
+
+    // COLUMNS CHECK
+    // FIRST COLUMN CHECK
+    if (cell11.textContent === cell21.textContent && cell11.textContent === symbol && cell31.textContent === "-") {
+        cell31.textContent = computer;
+        return true;
+    }
+
+    if (cell11.textContent === cell31.textContent && cell11.textContent === symbol && cell21.textContent === "-") {
+        cell21.textContent = computer;
+        return true;
+    }
+
+    if (cell21.textContent === cell31.textContent && cell21.textContent === symbol && cell11.textContent === "-") {
+        cell11.textContent = computer;
+        return true;
+    }
+
+    // SECOND COLUMN CHECK
+    if (cell12.textContent === cell22.textContent && cell12.textContent === symbol && cell32.textContent === "-") {
+        cell32.textContent = computer;
+        return true;
+    }
+
+    if (cell12.textContent === cell32.textContent && cell12.textContent === symbol && cell22.textContent === "-") {
+        cell22.textContent = computer;
+        return true;
+    }
+
+    if (cell22.textContent === cell32.textContent && cell22.textContent === symbol && cell12.textContent === "-") {
+        cell12.textContent = computer;
+        return true;
+    }
+
+    // THIRD COLUMN CHECK
+    if (cell13.textContent === cell23.textContent && cell13.textContent === symbol && cell33.textContent === "-") {
+        cell33.textContent = computer;
+        return true;
+    }
+
+    if (cell13.textContent === cell33.textContent && cell13.textContent === symbol && cell23.textContent === "-") {
+        cell23.textContent = computer;
+        return true;
+    }
+
+    if (cell23.textContent === cell33.textContent && cell23.textContent === symbol && cell13.textContent === "-") {
+        cell13.textContent = computer;
+        return true;
+    }
+
+    return false;
+}
+
+function computeSmartMoveWrapper() {
+    return computeSmartMove(computer) || computeSmartMove(player);
+}
+
 function validMove(row, col) {
     const cell = document.querySelector(`#cell${row}${col}`);
     if (cell.textContent === "-") {
@@ -167,15 +318,15 @@ function checkGameStatus() {
             computePlayerMove();
         }
         else {
-            let row = randomMove();
-            let col = randomMove();
+            if (!computeSmartMoveWrapper(computer)) {
+                let row = randomMove();
+                let col = randomMove();
 
-            while (!validMove(row, col)) {
-                row = randomMove();
-                col = randomMove();
+                while (!validMove(row, col)) {
+                    row = randomMove();
+                    col = randomMove();
+                }
             }
-
-            console.log(`COMPUTER MOVE: ${row} ${col}`);
 
             --freeCells;
             turn = 0;
